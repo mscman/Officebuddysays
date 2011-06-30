f746106b66a08c4745e53a405bcb7c061091083e
Andy Howard <ahoward@purdue.edu>
2011-06-29 22:47:58 -0400

    Fixed spacing issues

--- officebuddysays.pl
+++ officebuddysays.pl
@@ -13,39 +13,39 @@ $extension_mode = $EM_SCRIPT_OFF;
 
 $handle = sub {
         my $ref = shift;
-	if ($ref->{'user'}->{'protected'} eq 'true') {
-		return 0;
-	}
+    if ($ref->{'user'}->{'protected'} eq 'true') {
+        return 0;
+    }
 
-	my $sn = &descape($ref->{'user'}->{'screen_name'});
-	print $sn;
-	if ($sn eq $store->{'dontecho'}) {
-		return 0;
-	}
+    my $sn = &descape($ref->{'user'}->{'screen_name'});
+    print $sn;
+    if ($sn eq $store->{'dontecho'}) {
+        return 0;
+    }
 
 
-	#retweet!
-	my $string = "RT \@$sn " . &descape($ref->{'text'});
-	#don't retweet the hashtag too...
-	$string =~ s/#stuffmyofficebuddysays//ig;
-	$string =~ s/#thingsmyofficematesays//ig;
-	$string =~ s/#thingsmybosssays//ig;
-	$string =~ s/#stuffmybosssays//ig;
-	#print for debugging
-	print $stdout ("Publishing: ", $string, "\n");
-	&updatest($string);
-	&defaulthandle($ref);
-	return 1;
+    #retweet!
+    my $string = "RT \@$sn " . &descape($ref->{'text'});
+    #don't retweet the hashtag too...
+    $string =~ s/#stuffmyofficebuddysays//ig;
+    $string =~ s/#thingsmyofficematesays//ig;
+    $string =~ s/#thingsmybosssays//ig;
+    $string =~ s/#stuffmybosssays//ig;
+    #print for debugging
+    print $stdout ("Publishing: ", $string, "\n");
+    &updatest($string);
+    &defaulthandle($ref);
+    return 1;
 };
 
 $conclude = sub {
     #track last tweet we saw
     print $stdout "LIB: writing out: $last_id\n";
     if(open(S, ">".$store->{'master'})) {
-	print S $last_id;
-	close(S);
+    print S $last_id;
+    close(S);
     } else {
-	print $stdout "LIB: failure to write: $!\n";
+        print $stdout "LIB: failure to write: $!\n";
     }
     &defaultconclude;
 };
